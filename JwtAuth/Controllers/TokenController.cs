6c79da2486b06f1da6e28e635da82b33e85fa9f0
kki2020 <kki2020@daum.net>
2023-05-29 17:03:47 +0900

    초기 커밋.

---
+++ JwtAuth/Controllers/TokenController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuth.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[AllowAnonymous]
+public class TokenController:ControllerBase
+{
+    private const string TokenSecret = "dotnetdevforeverdotnetdevforeverdotnetdevforeverdotnetdevforeverdotnetdevforever";
+
+    [HttpGet]
+    [Route("Token")]
+    public IActionResult GenerateToken()
+    {
+        var tokenhandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(TokenSecret);
+        var timespan = TimeSpan.FromHours(8);
+        var claims = new List<Claim>
+        {
+            new (JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+            new (JwtRegisteredClaimNames.Sub,"dotnetdev"),
+            new (JwtRegisteredClaimNames.Email,"dot@korea.com"),
+            new ("Userid","blueBird"),
+            new ("admin","true"),
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.Add(timespan),
+            Issuer = "https://dotnetdeve.com",
+            Audience = "https://dotnetdeve.com/bluebird",
+            SigningCredentials =new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+        };
+
+        var token = tokenhandler.CreateToken(tokenDescriptor);
+        var jwt = tokenhandler.WriteToken(token);
+        return Ok(jwt);
+    }
+    
+}
+
+
+
+public class TokenGenerateRequest
+{
+
+}
---
+++ JwtAuth/Program.cs
@@ -0,0 +1,58 @@
+using JwtAuth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var config = builder.Configuration;
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x=> {
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = config["JwtSettings:Issuer"],
+        ValidAudience = config["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience= true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey =true
+
+    };
+});
+
+builder.Services.AddAuthorization(option =>
+{
+    option.AddPolicy(IdentityData.AdminUserClaimName, p => p.RequireClaim(IdentityData.AdminUserClaimName,"true"));
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
